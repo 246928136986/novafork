ffad4028f97f0a4cbeba9bfd29e3ce1f5d1864c7
246928136986 <math.trigonometry.alt.291@gmail.com>
2025-02-10 20:11:23 -0600

    Update mediaplayer.js

--- media/mediaplayer/mediaplayer.js
+++ media/mediaplayer/mediaplayer.js
@@ -118,7 +118,7 @@ async function getMovieEmbedUrl(mediaId, provider, apiKey, language = null) {
         case 'vidlinkdub':
             return `https://vidlink.pro/movie/${mediaId}?player=jw&multiLang=true&primaryColor=${primaryColor}&secondaryColor=${secondaryColor}&iconColor=${iconColor}`;
         case 'vidsrcnl':
-            return `https://player.vidsrc.nl/embed/movie/${mediaId}`;
+            return `https://vidsrc.vip/stellar.php?id=${mediaId}`;
         case 'vidsrc.rip':
             return `https://vidsrc.rip/embed/movie/${mediaId}`;
         case 'vidbinge':
